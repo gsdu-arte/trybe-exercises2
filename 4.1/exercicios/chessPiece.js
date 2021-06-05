6ee7ea110cfe86db47b1a819db7b22dda1ec3863
gsdu-arte <gabi15duarte@hotmail.com>
2021-06-05 15:34:28 -0300

    Adiciona o passo a passo requisito 5

--- 4.1/exercicios/chessPiece.js
+++ 4.1/exercicios/chessPiece.js
@@ -1,3 +1,6 @@
+// Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+// Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+
 let piece = 'KniGHt'
 let chessPiece = piece.toLowerCase()
 switch (chessPiece) {
