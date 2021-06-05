5cf50266439b7f330bb80f44aac482366c04702a
gsdu-arte <gabi15duarte@hotmail.com>
2021-06-05 16:57:13 -0300

    Alteraçoes feitas no requisito 10

--- 4.1/exercicios/profitOverProduct.js
+++ 4.1/exercicios/profitOverProduct.js
@@ -1,10 +1,14 @@
+// Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%.
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+
 let custo = 100;
 let precoVenda = 200;
-let impostoSobreCusto = (custo * 0.2)
-let valorCustoTotal = custo + impostoSobreCusto
-let lucro = precoVenda - valorCustoTotal
+let impostoSobreCusto = (custo * 0.2);
+let valorCustoTotal = custo + impostoSobreCusto;
+let lucro = precoVenda - valorCustoTotal;
 if (custo < 0 || precoVenda < 0) {
-  console.log('erro')
+  console.log('erro');
 }
-let vendeMil = 1000 * lucro
-console.log(vendeMil)
+let vendeMil = 1000 * lucro;
+console.log(vendeMil);
