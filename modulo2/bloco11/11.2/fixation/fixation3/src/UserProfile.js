4d77e7517ded56678af62fec0cfa8d249017bc1e
gsdu-arte <gabi15duarte@hotmail.com>
2021-07-26 12:25:28 -0300

    Resoluçao fixation3

---
+++ modulo2/bloco11/11.2/fixation/fixation3/src/UserProfile.js
@@ -0,0 +1,21 @@
+import { div } from 'prelude-ls';
+import React from 'react';
+import Image from './Image';
+
+class UserProfile extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          {this.props.user.name}
+        </p>
+        <p>
+          {this.props.user.email}
+        </p>
+        <Image source={this.props.user.avatar} alternativeText="User avatar"/>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
